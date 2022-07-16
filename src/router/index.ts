919f756cb0299456ee5ccd5623778509d6bafe3a
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-17 00:46:59 +0530

    #commit : Login component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,27 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '',
     component: () => import('@/views/Layout.vue'),
-  }
-]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/register',
+    component: () => import('@/views/Register.vue'),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
